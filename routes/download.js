ed1bfeac1f5151f07e3a8bdf461219d3a1158105
yuniyiqi23 <378532514@qq.com>
2018-03-20 10:13:14 +0800

    1.提交代码

--- routes/download.js
+++ routes/download.js
@@ -4,10 +4,11 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('pages/download_form', {title: 'Download'});
+    res.send('/pages/download_form')
 });
 
-router.post("/download", function (req, res) {
-    res.render("pages/download_success", {filepath: req.body.filepath});
-});
+// router.post("/download", function (req, res) {
+//     res.render("pages/download_success", {filepath: req.body.filepath});
+// });
 
 module.exports = router;
