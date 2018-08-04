e23da720dcac31f9c9cae38953276c6fa8177a3b
Adam <378532514@qq.com>
2018-08-04 21:06:43 +0800

    优化UI

---
+++ public/javascripts/initpaging.js
@@ -0,0 +1,17 @@
+/**
+  * 纯粹的JS分页插件，代码缺点：JS操作DOM冗余太多，太繁琐
+  */
+pageUtil.initPage('page', {
+    totalCount: '<%= postsCount%>',//总页数，一般从回调函数中获取。如果没有数据则默认为1页
+    curPage: 1,//初始化时的默认选中页，默认第一页。如果所填范围溢出或者非数字或者数字字符串，则默认第一页
+    showCount: 9,//分页栏显示的数量
+    pageSizeList: [5, 10, 15, 20, 25, 30],//自定义分页数，默认[5,10,15,20,50]
+    defaultPageSize: 1,//默认选中的分页数,默认选中第一个。如果未匹配到数组或者默认数组中，则也为第一个
+    isJump: true,//是否包含跳转功能，默认false
+    isPageNum: false,//是否显示分页下拉选择，默认false
+    isPN: true,//是否显示上一页和下一面，默认true
+    isFL: false,//是否显示首页和末页，默认true
+    jump: function (curPage, pageSize) {//跳转功能回调，传递回来2个参数，当前页和每页大小。如果没有设置分页下拉，则第二个参数永远为0。这里的this被指定为一个空对象，如果回调中需用到this请自行使用bind方法
+        //console.log(curPage, pageSize);
+    },
+});
