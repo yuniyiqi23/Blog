741ed9119401f42a72f87a8f7aa98396f570ab78
Adam <378532514@qq.com>
2018-10-22 15:01:38 +0800

    用户登录失败，保存用户名

--- routes/signin.js
+++ routes/signin.js
@@ -7,7 +7,7 @@ const UserModel = require('../models/users');
 const checkNotLogin = require('../middlewares/check').checkNotLogin;
 
 router.get('/', checkNotLogin, function (req, res, next) {
-    res.render('signin');
+    res.render('signin', { username: null });
 });
 
 router.post('/', checkNotLogin, function (req, res, next) {
@@ -15,30 +15,30 @@ router.post('/', checkNotLogin, function (req, res, next) {
     const password = req.body.password;
 
     //校验参数
-    try{
-        if(!name.length){
+    try {
+        if (!name.length) {
             throw new Error('请填写用户名！');
         }
-        if(!password.length){
+        if (!password.length) {
             throw new Error('请填写密码！');
         }
-    }catch (e){
-        req.flash('error', e.message);
-        return res.redirect('back');
+    } catch (e) {
+        // req.flash('error', e.message);
+        return res.render('signin', { username: name, error: e.message });
     }
 
     UserModel.getUserByName(name)
         .then(function (user) {
-            if(!user){
-                req.flash('error', '用户不存在！');
-                return res.redirect('back');
+            if (!user) {
+                // req.flash('error', '用户不存在！');
+                return res.render('signin', { username: name, error: '用户不存在！' });
             }
             //检查密码是否匹配
-            bcrypt.compare(password, user.password, function(err, result) {
+            bcrypt.compare(password, user.password, function (err, result) {
                 // result === true
-                if(result !== true){
-                    req.flash('error', '用户名或是密码错误！');
-                    return res.redirect('back');
+                if (result !== true) {
+                    // req.flash('error', '用户名或是密码错误！');
+                    return res.render('signin', { username: name, error: '用户名或是密码错误！' });
                 }
                 req.flash('success', '登录成功！');
                 // 用户信息写入 session
