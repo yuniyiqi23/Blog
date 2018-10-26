4905280b42cfdac119f99318314404a03d8eb429
Adam <378532514@qq.com>
2018-10-26 17:04:09 +0800

    pm2 每天定时重启

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,10 +7,10 @@ module.exports = {
 		// 最大限度地使用CPUs
 		instances: "max",
 		exec_mode: "cluster",
-		out_file: "blog_out.log",
-		error_file: "blog_error.log",
-		merge_logs: true,
-		log_date_format: "YYYY-MM-DD HH:mm:ss",
+		// 最小运行时间，如果程序在这个时间内退出，会被认为错误，自动重启
+		min_uptime: "120s", 
+		// 定时重启 （*/30 * * * *）每隔30分钟重启一次，（0 10 9 * * *） 每天9:10重启一次 
+		cron_restart: "0 1 1 * * *", 
 		env: {
 			"PORT": 3000,
 			"NODE_ENV": "development",
@@ -18,7 +18,13 @@ module.exports = {
 		env_production: {
 			"PORT": 3001,
 			"NODE_ENV": "production"
-		}
+		},
+		out_file: "~./logs/blog_out.log",
+		error_file: "./logs/blog_error.log",
+		merge_logs: true,
+		log_date_format: "YYYY-MM-DD HH:mm:ss",
+		// 启用/禁用 项目崩溃或退出时自动重启
+		autorestart: false, 
 	}],
 
 	// deploy : {
