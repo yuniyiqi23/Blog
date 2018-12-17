97a10ee64ac52ee51a8bcea724040a0030861578
Adam <378532514@qq.com>
2018-12-17 10:37:37 +0800

    draft

--- test/add.js
+++
@@ -1,7 +0,0 @@
-var zaphod = {name: 'Zaphod', age: 42};
-var marvin = {name: 'Marvin', age: 420000000000};
-
-zaphod.sayHello = function(){
-  return "Hi, I'm " + this.name;
-}
-marvin.sayHello = zaphod.sayHello;
