9229488b455dd3a64cb01dd5ecf55d52dc4d7459
adam <378532514@qq.com>
2019-03-13 18:56:26 +0800

    处理跨域问题

--- app.js
+++ app.js
@@ -87,6 +87,20 @@ app.get('/api/uptoken', function(req, res) {
     }
 });
 
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By", ' 3.2.1')
+        //这段仅仅为了方便返回json而已
+    res.header("Content-Type", "application/json;charset=utf-8");
+    if(req.method == 'OPTIONS') {
+        //让options请求快速返回
+        res.sendStatus(200); 
+    } else { 
+        next(); 
+    }
+});
 
 
 app.enable('trust proxy'); // only if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
