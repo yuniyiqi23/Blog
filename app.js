daef697e1f4ab70ed661edb9f70f1b1149dd226d
adam <378532514@qq.com>
2019-03-13 19:00:03 +0800

    跨域

--- app.js
+++ app.js
@@ -93,7 +93,7 @@ app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
     res.header("X-Powered-By", ' 3.2.1')
         //这段仅仅为了方便返回json而已
-    res.header("Content-Type", "application/json;charset=utf-8");
+    // res.header("Content-Type", "application/json;charset=utf-8");
     if(req.method == 'OPTIONS') {
         //让options请求快速返回
         res.sendStatus(200); 
